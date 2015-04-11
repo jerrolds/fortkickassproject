8683797966e0d927c63d1c8de91dfdda6dccaa42
jerrolds <jerrold.simbulan@hotmail.com>
2015-04-11 16:00:40 -0500

    jquery working

--- CustomCables/App_Start/BundleConfig.cs
+++ CustomCables/App_Start/BundleConfig.cs
@@ -9,7 +9,11 @@ namespace CustomCables
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.slideme2.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                    "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,7 +29,8 @@ namespace CustomCables
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/slideme.css"));
         }
     }
 }
